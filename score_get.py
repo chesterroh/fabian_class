7daba6b6efa2ac3214144566d2ebf6d6caafea91
Chester Jungseok Roh <chester.roh@gmail.com>
2018-01-13 11:03:51 +0900

    score_get modified

--- score_get.py
+++ score_get.py
@@ -7,18 +7,12 @@ with open('score_data.pickle','rb') as fp:
     score = pickle.load(fp)
 
 for (subj,data) in score.items():
-    print("%s: mean: %f, std: %f" % (subj, np.mean(data),np.std(data)))
+    print("%s: mean: %f, std: %f, var: %f" % (subj, np.mean(data),np.std(data),np.var(data)))
+    print("%s: above 90 %d" % (subj,np.sum( data >= 90 )))
+    print("%s: below 50 %d" % (subj,np.sum( data <= 50 )))
 
-sum = 0
-num = 0 
-
-for i in score['subj1']:
-    sum += i
-    num += 1
-
-print(num)
-print(sum)
-print(sum/num)
+scores = np.concatenate( [i for _,i in score.items()] )
+print("Total mean: %f, variance: %f, standard deviation: %f" % (np.mean(scores),np.var(scores),np.std(scores)))
 
 
     
